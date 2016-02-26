dbd8be60aaa142cb856693b61b084793003ab524
Lian Hong(Sam) Lee <llee@pivotal.io>
2016-02-26 14:57:57 -0800

    add yml

---
+++ build_postgresql.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+#cd postgresql-9.1.20
+./configure
+gmake
+su
+gmake install
+adduser postgres
+mkdir /usr/local/pgsql/data
+chown postgres /usr/local/pgsql/data
+su - postgres
+/usr/local/pgsql/bin/initdb -D /usr/local/pgsql/data
+/usr/local/pgsql/bin/postgres -D /usr/local/pgsql/data >logfile 2>&1 &
+/usr/local/pgsql/bin/createdb test
+/usr/local/pgsql/bin/psql test
