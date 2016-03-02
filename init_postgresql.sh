fbd1da848265f1e4c3d20c8d2b30a59b36c1dd0b
Lian Hong(Sam) Lee <llee@pivotal.io>
2016-03-01 17:50:05 -0800

    update bash scripts v4

--- init_postgresql.sh
+++ init_postgresql.sh
@@ -8,7 +8,7 @@ echo "starting logs"
 
 su - postgres -c "/usr/local/pgsql/bin/postgres -D /usr/local/pgsql/data >logfile 2>&1 &"
 echo "creating test DB..."
-su - postgres -c "/usr/local/pgsql/bin/createdb test"
+su - postgres -c "/usr/local/pgsql/bin/createdb test -U postgres"
 
 echo "chance to test DB"
 su - postgres -c "/usr/local/pgsql/bin/psql test"
