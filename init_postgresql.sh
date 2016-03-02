31a4c28977bdb15ebab74a7c557f03b9f1c50ef8
Lian Hong(Sam) Lee <llee@pivotal.io>
2016-03-01 16:37:44 -0800

    update bash scripts v3

--- init_postgresql.sh
+++ init_postgresql.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 set -e
-
+echo "Starting postgres"
 #is_init=`su - postgres -c 'echo "ps -ef" | psql | grep dcd_odasa | wc -l'`
 su - postgres -c "/usr/local/pgsql/bin/initdb -D /usr/local/pgsql/data"
+echo "starting logs"
+
 su - postgres -c "/usr/local/pgsql/bin/postgres -D /usr/local/pgsql/data >logfile 2>&1 &"
+echo "creating test DB..."
 su - postgres -c "/usr/local/pgsql/bin/createdb test"
+
+echo "chance to test DB"
 su - postgres -c "/usr/local/pgsql/bin/psql test"
 
 exit
