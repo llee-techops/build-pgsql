4fa182accb47b11506fc85625120a42b891a6ebd
Lian Hong(Sam) Lee <llee@pivotal.io>
2016-03-03 15:50:25 -0800

    update paths and bash scripts v7

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,9 +3,9 @@
 set -e
 
 adduser --disabled-password --gecos ""  postgres
-mkdir -p /usr/local/pgsql/data
-chown -R postgres /usr/local/pgsql
+#mkdir -p /usr/local/pgsql/data
+#chown -R postgres /usr/local/pgsql/data
 chown -R postgres ./git-postgres
-su postgres -c "./ci-scripts/scripts/build_postgresql.sh"
-#./ci-scripts/scripts/init_postgresql.sh
+#su postgres -c "./ci-scripts/scripts/build_postgresql.sh"
+./ci-scripts/scripts/build_postgresql.sh
 exit
--- scripts/build_postgresql.sh
+++ scripts/build_postgresql.sh
@@ -5,22 +5,23 @@ set -e
 cd git-postgres
 ./configure
 gmake
-#echo "========= running gmake check ============="
-#gmake check
+
+echo "========= running gmake check ============="
+gmake check
 
 # su
 echo "========= running gmake install ============="
 gmake install
 
 #adduser --disabled-password --gecos ""  postgres
-#mkdir /usr/local/pgsql/data
-#chown postgres /usr/local/pgsql/data
+mkdir /usr/local/pgsql/data
+chown -R postgres /usr/local/pgsql/data
 
-echo "Starting postgres"
-/usr/local/pgsql/bin/initdb -A -D /usr/local/pgsql/data
+#echo "Starting postgres"
+#/usr/local/pgsql/bin/initdb -D /usr/local/pgsql/data
 
-echo "start postgres and logs"
-/usr/local/pgsql/bin/pg_ctl -D /usr/local/pgsql/data -l logfile start
+#echo "start postgres and logs"
+#/usr/local/pgsql/bin/pg_ctl -D /usr/local/pgsql/data -l logfile start
 
 echo "===============   check postgres installations  ======================="
 echo "======================================================================="
