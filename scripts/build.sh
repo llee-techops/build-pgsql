ee71f8beb799c6f13c11c0376e7c3602f5e423b7
Lian Hong(Sam) Lee <llee@pivotal.io>
2016-03-03 12:04:25 -0800

    run install script as postgres v3

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,7 +5,7 @@ set -e
 adduser --disabled-password --gecos ""  postgres
 mkdir -p /usr/local/pgsql/data
 chown -R postgres /usr/local/pgsql/data
-
+chown -R postgres ./git-postgres
 su postgres -c "./ci-scripts/scripts/build_postgresql.sh"
 #./ci-scripts/scripts/init_postgresql.sh
 exit
