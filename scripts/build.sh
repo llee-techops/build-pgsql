026ae59481d678ffadfb50dd1a0aba5f7e6e0575
Lian Hong(Sam) Lee <llee@pivotal.io>
2016-03-03 11:33:36 -0800

    update paths and bash scripts v3

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,8 +3,8 @@
 set -e
 
 adduser --disabled-password --gecos ""  postgres
-mkdir /usr/local/pgsql/data
-chown postgres /usr/local/pgsql/data
+mkdir -p /usr/local/pgsql/data
+chown -R postgres /usr/local/pgsql/data
 
 su - postgres -c "./ci-scripts/scripts/build_postgresql.sh"
 #./ci-scripts/scripts/init_postgresql.sh
